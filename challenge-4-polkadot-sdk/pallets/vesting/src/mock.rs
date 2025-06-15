0b5a8f5cfa9dda38391a147d4fed52deea035210
ical10 <mochhusnir@gmail.com>
2025-06-15 20:54:01 +0800

    Test: Created basic testing env and added basic tests

--- challenge-4-polkadot-sdk/pallets/vesting/src/mock.rs
+++ challenge-4-polkadot-sdk/pallets/vesting/src/mock.rs
@@ -3,16 +3,15 @@
 #![cfg(test)]
 
 use super::*;
-use polkadot_sdk::{frame_support::{
-	construct_runtime, derive_impl, parameter_types,
-	traits::{ConstU32, ConstU64, EnsureOrigin},
-}, sp_runtime::traits::ConvertInto};
-
 use polkadot_sdk::{
-	polkadot_sdk_frame::runtime::prelude::*,
-	*,
+    frame_support::{
+        construct_runtime, derive_impl, parameter_types,
+        traits::{ConstU32, ConstU64, EnsureOrigin},
+    },
+    sp_runtime::traits::ConvertInto,
 };
 
+use polkadot_sdk::{polkadot_sdk_frame::runtime::prelude::*, *};
 
 use polkadot_sdk::sp_runtime::{traits::IdentityLookup, BuildStorage};
 
@@ -22,48 +21,46 @@ pub type AccountId = u128;
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl polkadot_sdk::frame_system::Config for Runtime {
-	type AccountId = AccountId;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Block = Block;
-	type AccountData = pallet_balances::AccountData<Balance>;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type AccountData = pallet_balances::AccountData<Balance>;
 }
 
 type Balance = u64;
 
 impl pallet_balances::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
-	type Balance = Balance;
-	type DustRemoval = ();
-	type ExistentialDeposit = ConstU64<1>;
-	type AccountStore = frame_system::Pallet<Runtime>;
-	type MaxLocks = ();
-	type MaxReserves = ();
-	type ReserveIdentifier = [u8; 8];
-	type WeightInfo = ();
-	type RuntimeHoldReason = RuntimeHoldReason;
-	type RuntimeFreezeReason = RuntimeFreezeReason;
-	type FreezeIdentifier = [u8; 8];
-	type MaxFreezes = ();
-	type DoneSlashHandler = ();
+    type Balance = Balance;
+    type DustRemoval = ();
+    type ExistentialDeposit = ConstU64<1>;
+    type AccountStore = frame_system::Pallet<Runtime>;
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type WeightInfo = ();
+    type RuntimeHoldReason = RuntimeHoldReason;
+    type RuntimeFreezeReason = RuntimeFreezeReason;
+    type FreezeIdentifier = [u8; 8];
+    type MaxFreezes = ();
+    type DoneSlashHandler = ();
 }
 
-
 impl Config for Runtime {
-	type Currency = PalletBalances;
-	type MinVestedTransfer = ConstU64<5>;
-	type MaxVestingSchedules = ConstU32<2>;
+    type Currency = PalletBalances;
+    type MinVestedTransfer = ConstU64<5>;
+    type MaxVestingSchedules = ConstU32<2>;
     type BlockNumberToBalance = ConvertInto;
-
 }
 
 type Block = frame_system::mocking::MockBlock<Runtime>;
 
 construct_runtime!(
-	pub enum Runtime {
-		System: frame_system,
-		Vesting: vesting,
-		PalletBalances: pallet_balances,
-	}
+    pub enum Runtime {
+        System: frame_system,
+        Vesting: vesting,
+        PalletBalances: pallet_balances,
+    }
 );
 
 pub const ALICE: AccountId = 1;
@@ -77,20 +74,25 @@ pub const CHARLIE_BALANCE: u64 = 50;
 pub struct ExtBuilder;
 
 impl ExtBuilder {
-	pub fn build() -> sp_io::TestExternalities {
-		let mut t = frame_system::GenesisConfig::<Runtime>::default()
-			.build_storage()
-			.unwrap();
+    pub fn build() -> sp_io::TestExternalities {
+        let mut t = frame_system::GenesisConfig::<Runtime>::default()
+            .build_storage()
+            .unwrap();
 
-		pallet_balances::GenesisConfig::<Runtime> {
-			balances: vec![(ALICE, ALICE_BALANCE), (CHARLIE, CHARLIE_BALANCE)],
-			..Default::default()
-		}
-		.assimilate_storage(&mut t)
-		.unwrap();
+        pallet_balances::GenesisConfig::<Runtime> {
+            balances: vec![(ALICE, ALICE_BALANCE), (CHARLIE, CHARLIE_BALANCE)],
+            ..Default::default()
+        }
+        .assimilate_storage(&mut t)
+        .unwrap();
 
+        let mut ext: sp_io::TestExternalities = t.into();
 
-		t.into()
-	}
+        // Set initial block number
+        ext.execute_with(|| {
+            System::set_block_number(100);
+        });
+
+        ext
+    }
 }
-
